eb60f7e954eb7f695731528b13dfb40c64873e52
Rasha ali <rashaali84@gmail.com>
2020-02-10 19:02:07 +0100

    solution

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,9 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+//  concatenate two strings together 
+// - How many parameters does the function take? two
+// - What is the function name?s
+// - Where is the function called? with what parameters?s , 2 parameters strings
+// - What does the function return? one string
+// - How can this function be improved? we can remove it  and just do the + operator  .
